18c7b4c450b5b677ee07b9b523de87cfd30639e4
iniferdy <renggasb73@gamil.com>
2024-02-29 07:52:46 +0700

    intro

--- lib/app/modules/intro/views/intro_view.dart
+++ lib/app/modules/intro/views/intro_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/intro_controller.dart';
 
 class IntroView extends GetView<IntroController> {
@@ -9,14 +10,30 @@ class IntroView extends GetView<IntroController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('IntroView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'IntroView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bg_intro.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/LogoPerpusAsli.png'),
+              SizedBox(height: 225),
+              ElevatedButton(
+                onPressed: ()=>Get.toNamed(Routes.LOGIN),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: TextStyle(fontSize: 20),
+                  backgroundColor: Color(0XFFFF7A00),
+                ),
+                child: Text('NEXT'),
+              ),
+            ],
+          ),
         ),
       ),
     );
